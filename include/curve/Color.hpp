033f6fa1e3d681dfaac43d24d7cbd196a4e35ee0
DomonkosSuranyi <suranyi.domi@gmail.com>
2017-01-26 20:57:45 +0100

    Color defined

---
+++ include/curve/Color.hpp
@@ -0,0 +1,17 @@
+#ifndef CURVE_COLOR_HPP_INC
+#define CURVE_COLOR_HPP_INC
+
+namespace curve
+{
+
+    struct Color
+    {
+        int r;
+        int g;
+        int b;
+        int a;
+    }
+
+}
+
+#endif
